8d9699ebb7343da4752f5c548307f1d4d7fe6dce
Boaz hoch <boazmier@gmail.com>
2023-01-19 13:21:32 +0200

    done

--- src/cli/adapter/cli.adapter.ts
+++ src/cli/adapter/cli.adapter.ts
@@ -11,25 +11,37 @@ interface Options {
     ex?: string;
 }
 
-const CLI_OPTIONS_STRINGS: {[index: string]: keyof Options} = {
-    srcComponentsGlob: "srcf",
-    tsconfigFilePath: "tsc",
-    template: "t",
-    excludeFiles: "ex"
-}
-
-const CLI_OPTIONS_STRINGS_DESC: {[k in keyof typeof CLI_OPTIONS_STRINGS]: string} = {
-    srcComponentsGlob: "components source folder path",
-    tsconfigFilePath: "tsconfig.json file path",
-    template: "template",
-    excludeFiles: "exclude files glob"
+const CLI_OPTIONS = {
+    srcComponentsGlob: {
+        required: true,
+        description: "components source folder path",
+        flag: "srcf",
+    },
+    tsconfigFilePath: {
+        required: true,
+        description: "tsconfig.json file path",
+        flag: "tsc",
+    },
+    template: {
+        required: false,
+        description: "template",
+        flag: "t",
+    },
+    excludeFiles: {
+        required: false,
+        description: "exclude files glob",
+        flag: "ex",
+    },
 }
 
 export class CliAdapter implements IStoriesBuilderAdapter {
     constructor(private program: Command = commanderProgram) {
-        Object.entries(CLI_OPTIONS_STRINGS).forEach(([key,value]) => {
-            this.program
-                .addOption(new Option(`-${value} <string>`, `${CLI_OPTIONS_STRINGS_DESC[key]}`))
+        Object.values(CLI_OPTIONS).forEach((value) => {
+            if(value.required) {
+                this.program.requiredOption(`-${value.flag} <string>`, `${value.description}`);
+                return;
+            }
+            this.program.addOption(new Option(`-${value.flag} <string>`, `${value.description}`));
         })
     }
 
@@ -42,10 +54,10 @@ export class CliAdapter implements IStoriesBuilderAdapter {
         }, {} as Options)
 
         return {
-            componentsSrcFilePath: opts[CLI_OPTIONS_STRINGS.srcComponentsGlob] || "",
-            tsconfigFilePath: opts[CLI_OPTIONS_STRINGS.tsconfigFilePath] || "",
-            template: opts[CLI_OPTIONS_STRINGS.temaplte] || "",
-            excludedSrcFileGlob: opts[CLI_OPTIONS_STRINGS.excludeFiles],
+            componentsSrcFilePath: opts[CLI_OPTIONS.srcComponentsGlob.flag as keyof Options] || "",
+            tsconfigFilePath: opts[CLI_OPTIONS.tsconfigFilePath.flag as keyof Options] || "",
+            template: opts[CLI_OPTIONS.template.flag as keyof Options] || "",
+            excludedSrcFileGlob: opts[CLI_OPTIONS.excludeFiles.flag as keyof Options],
         }
     }
 }
